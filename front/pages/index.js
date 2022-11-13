fda84e82efad409611303ea094b11cf20c4d993f
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-13 12:15:13 +0900

    環境構築

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import { Alert, Button } from "reactstrap";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello Project</Alert>
+        <Button color="primary">Hello from nextjs</Button>
+      </div>
+    </div>
+  );
+}
